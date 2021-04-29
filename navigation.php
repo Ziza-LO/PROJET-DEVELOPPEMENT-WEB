8889e1aee99452db898f3ad8dd0d14af234cc9b6
Ziza-LO <azizdabakh97@gmail.com>
2021-04-29 20:44:10 +0000

    barre nav

---
+++ navigation.php
@@ -0,0 +1,13 @@
+
+    <header>
+        <nav>
+            <img src="media/logo.png" id="logo">
+            <div>
+            <ul>
+            <a href="index.php" class="menu">Acceuil</a>
+            <a href="Ajouter.php" class="menu">Ajouter un produit</a>
+            <a href="Liste.php" class="menu">Liste des produits</a>
+            </ul>
+            </div>
+        </nav>
+    </header>
